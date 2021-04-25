ce569a07527d7a141c0479205aa1a05a537de772
Paulo Ricardo <palmeida.ipms@gmail.com>
2021-04-25 20:42:13 -0300

    Repo started

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b334658759e9227aea754c4afb8540d3'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '40d063d6-40c5-466e-808d-4b858da6afed'
+};
