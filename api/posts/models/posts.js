81ec8e7da5f4267734b5629eba0a3d45dd47ef9e
Paulo Ricardo <palmeida.ipms@gmail.com>
2021-05-08 22:17:27 -0300

    Add lifecycle

--- api/posts/models/posts.js
+++ api/posts/models/posts.js
@@ -9,7 +9,7 @@ const axios = require('axios');
 module.exports = {
     async afterCreate(result, data) {
         axios.post('https://api.netlify.com/build_hooks/6096cad03047e5c1b32923d1');
-    }
+    },
 
     async afterUpdate(result, params, data) {
         axios.post('https://api.netlify.com/build_hooks/6096cad03047e5c1b32923d1');
